ea15e03453468011f73e69c1ea161535485a8585
Lyubov Parfilova <cnlmode@gmail.com>
2021-09-07 16:24:09 +0400

    stepik course - task 1

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 import {Builder, By, Key, until} from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome.js';
 import path from 'path'
-//import firefox from 'selenium-webdriver/firefox.js';
-
 
 (async function example() {
-    //const serviceBuilder = new firefox.ServiceBuilder(getWebdriverAbsolutePath('geckodriver'));
-    //let driver = await new Builder().forBrowser('firefox').setFirefoxService(serviceBuilder).build();
-
     chrome.setDefaultService(new chrome.ServiceBuilder(getWebdriverAbsolutePath()).build());
     let driver = await new Builder().forBrowser('chrome').build();
 
     try {
-        // Navigate to Url
-        await driver.get('https://www.google.com');
+        const url = 'http://suninjuly.github.io/simple_form_find_task.html';
 
-        // Enter text "cheese" and perform keyboard action "Enter"
-        await driver.findElement(By.name('q')).sendKeys('cheese', Key.ENTER);
+        await driver.get(url);
 
-        let firstResult = await driver.wait(until.elementLocated(By.css('h3')), 50000);
-
-        console.log(await firstResult.getAttribute('textContent'));
+        await driver.findElement(By.tagName('input')).sendKeys('Ivan')
+        await driver.findElement(By.name('last_name')).sendKeys('Petrov')
+        await driver.findElement(By.className('city')).sendKeys('Smolensk')
+        await driver.findElement(By.id('country')).sendKeys('Smolensk')
+        
+        const button = await driver.findElement(By.css('button.btn'))
+        await button.sendKeys('Russia')
+        await button.click()
     }
     finally{
-        driver.quit();
+        console.log('waiting 3 seconds and then - close browser.');
+        setTimeout(() => driver.quit(), 30000);
     }
 })();
 
